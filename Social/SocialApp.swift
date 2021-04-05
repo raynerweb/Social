e0b055ed61dbceb7ca7fb90493a35d1dcac0dc17
rayner <raynerweb@gmail.com>
2021-04-05 19:47:43 -0300

    Initial Commit

---
+++ Social/SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialApp.swift
+//  Social
+//
+//  Created by rayner on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
